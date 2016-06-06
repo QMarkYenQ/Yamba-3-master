d4c1a0e388f601346827a2805eb4848be971e2bd
Mark <ywtean@gmail.com>
2016-06-06 15:52:50 +0800

    Initial commit

---
+++ app/src/main/java/com/marakana/android/yamba/StatusActivity.java
@@ -0,0 +1,22 @@
+package com.marakana.android.yamba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check if this activity was created before
+        if (savedInstanceState == null) {
+            // Create a fragment
+            StatusFragment fragment = new StatusFragment();
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment,
+                            fragment.getClass().getSimpleName()).commit();
+        }
+    }
+}
